1480273a71818ba5819b5cac07d9e6274c35ce19
JesnaJai <jesnajaison98@gmail.com>
2023-08-04 14:23:17 +0530

    first commit

---
+++ src/main/java/com/example/demo/SampleApplication.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@SpringBootApplication
+@EnableWebMvc
+public class SampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleApplication.class, args);
+	}
+
+}
